4608eb0560c487c6af901dd22c8b766ee9e8c26a
Ady_zxxy <161061040+noideafornickk@users.noreply.github.com>
2024-09-17 18:04:32 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é javaScript?',
+    ' JavaScript é uma linguagem de programação que permite a criação de interações dinâmicas em sites. Ela pode ser usada para adicionar efeitos em páginas web, como animações, formulários interativos e até jogos. Junto com HTML e CSS, é uma das principais tecnologias da web.'
+)
+
+criaCartao(
+    'Linguagem de marcação',
+    'Para que serve o HTML?',
+    'HTML (HyperText Markup Language) é a base de todas as páginas da web. Ele serve para estruturar o conteúdo de um site, criando elementos como títulos, parágrafos, links e imagens. Sem HTML, um site não teria uma estrutura visível.'
+)
+
+criaCartao(
+    'Estrutura de Estilo',
+    'O que é as CSS?',
+    'CSS (Cascading Style Sheets) é usado para controlar a aparência visual de uma página web. Com o CSS, é possível alterar cores, fontes, espaçamentos, layout e muito mais, permitindo que o site tenha um design atraente e responsivo.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Java?',
+    'Ao contrário de JavaScript, que é usado principalmente para adicionar interatividade em páginas web, Java é mais voltado para a criação de aplicativos completos, seja no ambiente desktop, móvel ou backend. Enquanto o JavaScript é uma linguagem interpretada que roda diretamente no navegador, o Java é compilado em bytecode e executado pela JVM.'
+)
